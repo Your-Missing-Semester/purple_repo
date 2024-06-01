622b48678dc12f60c37821ec67ac6368c86d9b2b
jonathan.s9522@gmail.com <jxnathan952@Jonathans-MBP-4.lan>
2024-05-31 23:56:55 -0400

    add reset password form with no interaction

---
+++ frontend/src/resetPswd.js
@@ -0,0 +1,63 @@
+import './form.css'
+function FormButton({size, display_name}) {
+    return (
+        <button
+            className = "form_btn"
+            height = {size}
+            width = {size}
+        >{display_name}</button>
+    );
+}
+
+function Logo() {
+    return (
+        <div
+        className = "logo"
+        >logo goes here</div>
+    );
+}
+
+function Textbox({name, type, display_name}) {
+    return (
+        <div className = "textbox">
+            <label for = {name}>{display_name}</label>
+            <input type = {type} name = {name} id = {name}/>    
+        </div>
+    );
+}
+
+export default function ResetPasswordForm() {
+    return (
+        <div className = "border">
+            <div className = "left_side">
+                <form>
+                    <Logo
+                        size = {200}
+                    />
+                    <Textbox
+                        name = {"old_password"}
+                        type = {"text"}
+                        display_name = {"Old Password:"}
+                    />
+                    <Textbox
+                        name = {"new_password"}
+                        type = {"text"}
+                        display_name = {"New Password:"}
+                    />
+                    <Textbox
+                        name = {"confirm_password"}
+                        type = {"text"}
+                        display_name = {"Confirm Password:"}
+                    />
+                    <FormButton
+                        size = {100} 
+                        display_name = {"Save Password"}
+                    />
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+
