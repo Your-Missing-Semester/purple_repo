6d97df506a35d948650e824092ec288b420fec05
cieloxroman <croman12@depaul.edu>
2024-06-07 21:41:21 -0500

    Updated SignIn and SignUp after reviewing pull req comments (also changed the file names to be camel case)

--- frontend/src/Signin.js
+++ frontend/src/Signin.js
@@ -1,49 +1,38 @@
 import styles from './auth.module.css';
 import classNames from 'classnames';
-//<img src={logo} className="App-logo" alt="logo" /> old logo
-//<header className="App-header">
-//        Hello
-//      </header>
-// 
-function Signin() {
+
+function SignIn() {
   return (
     <div className = {styles.auth}>
       <div className = {styles.main}>
         <div className = {styles.border}>
-        <header className = {styles.app}>
-                <a href="./"><img src="career_connect_logo.png" alt="logo" width ="200" hieght="200" /></a>
-                <br />
-                    Welcome back!
-                <br />
-            </header>
-            <br />
-            <header className = {styles.describeAuth}>Enter your login details below</header>
-            <br/>
-            <item className = {styles.subheaders}>
-                Email
-            </item>
-            <br/>
-            <input className = {styles.authInput} placeholder="example@gmail.com" type = "text" id="email" name="email" />
-            <br/>
-            <br/>
+          <a href="./"><img className = {styles.center} src="career_connect_logo.png" alt="logo" width ="200" hieght="200" /></a>
+          <header className={classNames(styles.app, styles.mb20)}>
+            Welcome back!
+          </header>
             
-            <item className = {styles.subheaders}>
-                Password
-            </item>
-            <br/>
-            <input className = {styles.authInput} placeholder="Enter your password" type = "text" id="email" name="email" />
-            <br/>
-            <p className = {styles.authP}>Don't have an account yet? <a href="https://react.dev/learn" target="_blank" rel="noreferrer">SIGN UP</a></p>
-            <button className={classNames(styles.accountButton, styles.app)}>LOGIN</button>
-            <br/>
-            <br/>
-            <p className = {styles.line}><span>or login with</span></p>
-            <a href="https://google.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="google_symbol.png" alt="google" width ="10%" hieght="10%"/></a>
-            <a href="https://apple.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="apple_symbol.png" alt="apple" width ="10%" hieght="10%"/></a>
+          <form action="">
+            <header className={classNames(styles.authType, styles.mb20)}>
+              Enter your login details below</header>
+              
+            <label className={classNames(styles.subheaders, styles.mb20)}>
+              Email</label>  
+            <input className={classNames(styles.authInput, styles.mb20)} placeholder="example@gmail.com" type = "text" id="email" name="email" />
+              
+            <label className={classNames(styles.subheaders, styles.mb20)}>
+            Password</label>
+            <input className={classNames(styles.authInput, styles.mb20)} placeholder="Enter your password" type = "text" id="email" name="email" />
+          </form>
+          <p className={classNames(styles.authP, styles.mb20)}>Don't have an account yet? <a href="https://react.dev/learn" target="_blank" rel="noreferrer">
+            SIGN UP</a></p>
+          <button className={classNames(styles.accountButton, styles.app, styles.mb20)}>LOGIN</button>
+          <p className = {styles.line}><span>or login with</span></p>
+          <a href="https://google.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="google_symbol.png" alt="google" width ="10%" hieght="10%"/></a>
+          <a href="https://apple.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="apple_symbol.png" alt="apple" width ="10%" hieght="10%"/></a>
         </div>
       </div>
     </div>
   );
 }
 
-export default Signin;
+export default SignIn;
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,53 +1,42 @@
 import styles from './auth.module.css';
 import classNames from 'classnames';
-// Sign up page
-function Signup() {
+
+function SignUp() {
   return (
     <div className = {styles.auth}>
-      
-      <main className = {styles.main}>
+      <div className = {styles.main}>
         <div className = {styles.border}>
-            <header className = {styles.app}>
-                <a href="./"><img src="career_connect_logo.png" alt="logo" width ="200" hieght="200" /></a>
-                <br />
-                    Start connecting today!
-                <br />
-            </header>
-            <br />
-            <header className = {styles.describeAuth}>Create an account</header>
-            <br/>
-            <item className = {styles.subheaders}>
-                Email
-            </item>
-            <br/>
-            <input className = {styles.authInput} placeholder="example@gmail.com" type = "text" id="email" name="email" />
-            <br/>
-            <br/>
-            
-            <item className = {styles.subheaders}>
-                Password
-            </item>
-            <br/>
-            <input className = {styles.authInput} placeholder="Enter your password" type = "text" id="email" name="email" />
-            <br/>
-            <br/>
-            <item className = {styles.subheaders}>
-                Confirm Password
-            </item>
-            <br/>
+          <a href="./"><img className = {styles.center} src="career_connect_logo.png" alt="logo" width ="200" hieght="200" /></a>
+          <header className={classNames(styles.app, styles.mb20)}>
+            Start connecting today!
+          </header>
+          <form action ="">
+            <header className={classNames(styles.authType, styles.mb20)}>
+              Create an account</header>
+
+            <label className={classNames(styles.subheaders, styles.mb20)}> 
+              Email </label>
+
+            <input className={classNames(styles.authInput, styles.mb20)} placeholder="example@gmail.com" type = "text" id="email" name="email" />
+                
+            <label className={classNames(styles.subheaders, styles.mb20)}>
+              Password</label>
+            <input className={classNames(styles.authInput, styles.mb20)} placeholder="Enter your password" type = "text" id="email" name="email" />
+                
+            <label className={classNames(styles.subheaders, styles.mb20)}>
+              Confirm Password</label>
+                
             <input className = {styles.authInput} placeholder="Confirm your password" type = "text" id="email" name="email" />
-            <p className = {styles.authP}>Already have an account? <a href="https://react.dev/learn" target="_blank" rel="noreferrer">LOGIN</a></p>
-            <button className={classNames(styles.accountButton, styles.app)}>SIGN UP</button>
-            <br/>
-            <br/>
-            <p className = {styles.line}><span>or sign up with</span></p>
-            <a href="https://google.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="google_symbol.png" alt="google" width ="10%" hieght="10%"/></a>
-            <a href="https://apple.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="apple_symbol.png" alt="apple" width ="10%" hieght="10%"/></a>
+          </form>
+          <p className = {styles.authP}>Already have an account? <a href="https://react.dev/learn" target="_blank" rel="noreferrer">LOGIN</a></p>
+          <button className={classNames(styles.accountButton, styles.app, styles.mb20)}>SIGN UP</button>
+          <p className = {styles.line}><span>or sign up with</span></p>
+          <a href="https://google.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="google_symbol.png" alt="google" width ="10%" hieght="10%"/></a>
+          <a href="https://apple.com" target="_blank" rel="noreferrer"><img className = {styles.signUpOption} src="apple_symbol.png" alt="apple" width ="10%" hieght="10%"/></a>
         </div>
-      </main>
-    
+      </div>
     </div>
   );
 }
 
-export default Signup;
+export default SignUp;
