62c7254db6ad636c1168bc04db1dc3557741a3fa
Hafsa <157250037+hferoz1@users.noreply.github.com>
2024-06-21 23:38:33 -0500

    Update LandingPage.js

--- LandingPage.js
+++ LandingPage.js
@@ -1,11 +1,11 @@
-import './LandingPage.css';
+import styles from './LandingPage.module.css';
 import classNames from 'classnames';
 
 function LandingPage() {
     return (
         <div>
             <header>
-                <div className="navBar">
+                <div className={styles.navBar}>
                     <a href="#home">HOME</a>
                     <a href="#signUp">SIGN UP</a>
                     <a href="#logIn">LOG IN</a>
@@ -13,9 +13,9 @@ function LandingPage() {
                 </div>
             </header>
             <h1>CAREER CONNECT</h1>
-            <div className="main-content">
-                <section className="container">
-                    <div className="content">
+            <div className={styles.mainContent}>
+                <section className={styles.container}>
+                    <div className={styles.content}>
                         <h2>Loren Ipsum</h2>
                         <p>
                             Lorem ipsum dolor sit amet, sed unum adversarium disputationi eu, aliquid conceptam mel ut. Vel modo nusquam ad, cu sea oratio legere ornatus. Quo ex ignota senserit, ei sea soluta fuisset. Sea ex impetus mediocrem intellegat. Sea ad enim nobis convenire, his brute velit ea, mel an clita reprimique.
@@ -24,39 +24,39 @@ function LandingPage() {
                             <button type="button">Sign up!</button>
                         </a>
                     </div>
-                    <img className="img1" src="career_connect_logo.png" alt="CC Logo" />
+                    <img className={styles.img1} src="career_connect_logo.png" alt="CC Logo" />
                 </section>
-                <section className="container2">
-                    <div className="content">
-                        <img className="img2" src="IMGblank.png" alt="Placeholder" />
+                <section className={styles.container2}>
+                    <div className={styles.content}>
+                        <img className={styles.img2} src="IMGblank.png" alt="Placeholder" />
                         <h3>Loren Ipsum</h3>
                         <p>
                             Lorem ipsum dolor sit amet, sed unum adversarium disputationi eu, aliquid conceptam mel ut. Vel modo nusquam ad, cu sea oratio legere ornatus. Quo ex ignota senserit, ei sea soluta fuisset. Sea ex impetus mediocrem intellegat. Sea ad enim nobis convenire, his brute velit ea, mel an clita reprimique.
                         </p>
                     </div>
-                    <div className="content">
-                        <img className="img3" src="IMGblank.png" alt="Placeholder" />
+                    <div className={styles.content}>
+                        <img className={styles.img3} src="IMGblank.png" alt="Placeholder" />
                         <h3>Loren Ipsum</h3>
                         <p>
                             Lorem ipsum dolor sit amet, sed unum adversarium disputationi eu, aliquid conceptam mel ut. Vel modo nusquam ad, cu sea oratio legere ornatus. Quo ex ignota senserit, ei sea soluta fuisset. Sea ex impetus mediocrem intellegat. Sea ad enim nobis convenire, his brute velit ea, mel an clita reprimique.
                         </p>
                     </div>
-                    <div className="content">
-                        <img className="img4" src="IMGblank.png" alt="Placeholder" />
+                    <div className={styles.content}>
+                        <img className={styles.img4} src="IMGblank.png" alt="Placeholder" />
                         <h3>Loren Ipsum</h3>
                         <p>
                             Lorem ipsum dolor sit amet, sed unum adversarium disputationi eu, aliquid conceptam mel ut. Vel modo nusquam ad, cu sea oratio legere ornatus. Quo ex ignota senserit, ei sea soluta fuisset. Sea ex impetus mediocrem intellegat. Sea ad enim nobis convenire, his brute velit ea, mel an clita reprimique.
                         </p>
                     </div>
                 </section>
-                <section className="container3">
-                    <div className="content">
+                <section className={styles.container3}>
+                    <div className={styles.content}>
                         <h3>Loren Ipsum</h3>
                         <p>
                             Lorem ipsum dolor sit amet, sed unum adversarium disputationi eu, aliquid conceptam mel ut. Vel modo nusquam ad, cu sea oratio legere ornatus. Quo ex ignota senserit, ei sea soluta fuisset. Sea ex impetus mediocrem intellegat. Sea ad enim nobis convenire, his brute velit ea, mel an clita reprimique.
                         </p>
                     </div>
-                    <img className="img5" src="IMGblank.png" alt="Placeholder" />
+                    <img className={styles.img5} src="IMGblank.png" alt="Placeholder" />
                 </section>
             </div>
             <footer>
