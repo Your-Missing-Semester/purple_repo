a3ea17bd98e2aca1f7ad9dbec75b37332a447ed6
Jonathan <jonathan.s9522@gmail.com>
2024-06-29 18:04:59 -0400

    add change username method on fe

--- frontend/src/resetUsername.js
+++ frontend/src/resetUsername.js
@@ -1,37 +1,117 @@
-import {Logo, Textbox, FormButton} from "./resetPswd.js";
+import {useState, useEffect} from 'react';
 import FormCSS from "./form.module.css";
+import axios from 'axios';
+
+
 
 export default function ResetUsernameForm() {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [newUsername, setNewUsername] = useState('');
+    const [message, setMessage] = useState('');
+
+    const checkUsername = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/users/${newUsername}`)
+            return response.data.exists;
+        } catch (err) {
+            console.error('error checking username: ', err)
+            return false;
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setMessage('');
+        const usernameExists = await checkUsername();
+        if (!email || !username || !newUsername) {
+            setMessage('fill in all required information')
+            return;
+        }
+        if (usernameExists) {
+            setMessage("username already exists")
+            return;
+        }
+        try {
+            const res = await axios.put(`http://localhost:8080/users/${username}`, {
+                email: email,
+                username: newUsername
+            });
+            if (res.data.success) {
+                setMessage('updated username successfully');
+                setUsername(newUsername);
+                setNewUsername('');
+                setUsername('');
+                setEmail('');
+            } 
+            else {
+                setMessage('failed updating username');
+            }
+        } catch (err) {
+            const {message} = err.response.data;
+            setMessage(message);
+            console.error('error updating username', err)
+        }
+    
+    }
+
     return (
         <div className = {FormCSS.border}>
             <div className = {FormCSS.left_side}>
                 <form>
-                    <Logo
-                        size = {100}
+                    <div>
+                    <img
+                        className = {FormCSS.logo}
+                        src = './careerConnectLogo.png' 
+                        alt = "logo"
+                        width = "100"
+                        height = "100"
                     />
-                    <h1>Reset Username</h1>
-                    <Textbox
-                        name = {"email"}
-                        type = {"email"}
-                        display_name = {"Email:"}
-                    />
-                    <Textbox
-                        name = {"new_username"}
-                        type = {"text"}
-                        display_name = {"New Username:"}
-                    />
-                    <Textbox
-                        name = {"confirm_username"}
-                        type = {"text"}
-                        display_name = {"Confirm Username:"}
-                    />
-                    <FormButton
-                        size = {100} 
-                        display_name = {"Save Changes"}
-                    />
+                    </div>
+                    <h1>Change Username</h1>
+                    <div className = {FormCSS.textbox}>
+                        <input 
+                            type = "email"
+                            name = "email" 
+                            id = "email" 
+                            value = {email}
+                            onChange = {(e) => setEmail(e.target.value)}
+                            placeholder = "Email:"
+                            required
+                        />    
+                    </div>
+                    <div className = {FormCSS.textbox}>
+                        <input 
+                            type = "text"
+                            name = "username" 
+                            id = "username" 
+                            value = {username}
+                            onChange = {(e) => setUsername(e.target.value)}
+                            placeholder = "Old Username: "
+                            required
+                        />    
+                    </div>
+                    <div className = {FormCSS.textbox}>
+                        <input 
+                            type = "text"
+                            name = "confirm_username" 
+                            id = "confirm_username" 
+                            value = {newUsername}
+                            onChange = {(e) => setNewUsername(e.target.value)}
+                            placeholder = "New Username:"
+                            required
+                        />    
+                    </div>
+                    <button
+                        className = {FormCSS.form_btn}
+                        onClick = {handleSubmit}
+                        height = "100"
+                        width = "100"
+                        >Save Changes
+                    </button>
+                    <p>{message}</p>
                 </form>
             </div>
-
         </div>
     )
 }
