fa89422e270cc861ad57eedd5e42a43cd3c13c38
Hafsa <157250037+hferoz1@users.noreply.github.com>
2024-07-04 19:24:11 -0500

    Deleted server directory to add updated files

--- server/prisma/migrations/20240701151138_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
-    "name" TEXT
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Post_email_key" ON "Post"("email");
