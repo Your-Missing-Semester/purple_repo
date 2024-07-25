4e237f8a64d2380f741f901f244989977bc7bb5b
Aakash Shah <121144051+vvkash@users.noreply.github.com>
2024-07-25 01:26:36 -0400

    resolved small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import ResetPasswordForm from './resetPswd.js';
 import ResetUsernameForm from './resetUsername.js';
 import FormPage from './formPage.js';
-import changePasswordRouter from '../../backend/routes/passwordRoute.js';
 
 import Signup from './components/authorization/SignUp.js';  
 import Signin from './components/authorization/SignIn.js';
 
-export default function App(){
+import NavBar from './components/LandingPage/NavBar.js'; 
+import Footer from './components/LandingPage/Footer.js'; 
+import LandingPage from './components/LandingPage/LandingPage.js'; 
+import './components/LandingPage/LandingPage.module.css'; 
+
+export default function App() {
   return (
     <>
-    <Routes>
-      <Route path = "/" element = {<FormPage/>}></Route>
-      <Route path = "/sign-up" element = {<Signup/>} ></Route>
-      <Route path = "/sign-in" element = {<Signin/>} ></Route>
-      <Route path = "/reset-username-form" element = {<ResetUsernameForm/>} ></Route>
-      <Route path = "/reset-password-form" element = {<ResetPasswordForm/>} ></Route>
-      <Route path = "/change-password-router" element = {<changePasswordRouter/>} ></Route>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/form" element={<FormPage />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/reset-username-form" element={<ResetUsernameForm />} />
+        <Route path="/reset-password-form" element={<ResetPasswordForm />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
-
